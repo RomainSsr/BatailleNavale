663eb05b7b336fff47afd3b74b9744a0e04d411a
RomainSsr <romain.ssr@eduge.ch>
2017-11-16 10:07:25 +0100

    dv

--- batailleNavale/frmMain.Designer.cs
+++ batailleNavale/frmMain.Designer.cs
@@ -400,6 +400,7 @@
             // 
             // frmMain
             // 
+            this.AcceptButton = this.btnSendMessage;
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1203, 973);
